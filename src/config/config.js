39e263b5c906ad92116cbd99ce87b7d0b85d4b96
anderson ocana <anderson.ocana@ort.edu.ar>
2025-11-26 22:07:17 -0300

    Update config.js --> defaults

--- src/config/config.js
+++ src/config/config.js
@@ -9,12 +9,15 @@ const {
   MYSQL_HOST,
   MYSQL_PORT,
   DIALECT,
-  SERVER_PORT,
-  SERVER_HOST,
+//  SERVER_PORT,
+//  SERVER_HOST,
   SUPABASE_URL,
   SUPABASE_API_KEY,
 } = process.env;
 
+const HOST = process.env.HOST || "0.0.0.0"
+const PORT =  process.env.PORT || 3000
+  
 const config = {
   MYSQL_DB,
   MYSQL_USER,
@@ -22,8 +25,8 @@ const config = {
   MYSQL_HOST,
   MYSQL_PORT,
   DIALECT,
-  SERVER_PORT,
-  SERVER_HOST,
+  SERVER_PORT:PORT,
+  SERVER_HOST:HOST,
   SUPABASE_URL,
   SUPABASE_API_KEY,
 };
